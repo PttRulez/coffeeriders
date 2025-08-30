be1a963bfcf22bd7eeabb1e0bfaad98af89fecbb
satoshi <sashafem@mail.ru>
2025-08-30 18:50:31 +0300

    yandex metrika added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,41 +35,43 @@
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+
   @routes
   @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
   @inertiaHead
 
+
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript">
+      (function(m, e, t, r, i, k, a) {
+          m[i] = m[i] || function() {
+              (m[i].a = m[i].a || []).push(arguments);
+          };
+          m[i].l = 1 * new Date();
+          for (var j = 0; j < document.scripts.length; j++) {
+              if (document.scripts[j].src === r) {
+                  return;
+              }
+          }
+          k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
+      })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=103954739', 'ym');
+
+      ym(103954739, 'init', {
+          ssr: true,
+          webvisor: true,
+          clickmap: true,
+          ecommerce: 'dataLayer',
+          accurateTrackBounce: true,
+          trackLinks: true
+      });
+  </script>
+  <noscript>
+    <div><img src="https://mc.yandex.ru/watch/103954739" style="position:absolute; left:-9999px;" alt="" /></div>
+  </noscript>
+  <!-- /Yandex.Metrika counter -->
 </head>
 <body class="font-sans antialiased">
 
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function(m, e, t, r, i, k, a) {
-        m[i] = m[i] || function() {
-            (m[i].a = m[i].a || []).push(arguments);
-        };
-        m[i].l = 1 * new Date();
-        for (var j = 0; j < document.scripts.length; j++) {
-            if (document.scripts[j].src === r) {
-                return;
-            }
-        }
-        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
-    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=103954739', 'ym');
-
-    ym(103954739, 'init', {
-        ssr: true,
-        webvisor: true,
-        clickmap: true,
-        ecommerce: 'dataLayer',
-        accurateTrackBounce: true,
-        trackLinks: true
-    });
-</script>
-<noscript>
-  <div><img src="https://mc.yandex.ru/watch/103954739" style="position:absolute; left:-9999px;" alt="" /></div>
-</noscript>
-<!-- /Yandex.Metrika counter -->
 @inertia
 </body>
 </html>
