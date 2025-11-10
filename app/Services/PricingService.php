271a6c827d05674cbd475341bcf90bd3be722127
satoshi <sashafem@mail.ru>
2025-11-10 22:03:11 +0300

    баг с PricingService незалогиненым юзером

--- app/Services/PricingService.php
+++ app/Services/PricingService.php
@@ -7,7 +7,7 @@ use App\Models\User;
 
 class PricingService
 {
-    public function basePrice(User $user, ServiceType $service): int
+    public function basePrice(User | null $user, ServiceType $service): int
     {
         return match ($service) {
             ServiceType::Cycling  => $user?->is_coffeerider ? 750 : 1500,
